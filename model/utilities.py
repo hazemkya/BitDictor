0be712be229d25d7f9d60e260c70c4ee28feb95b
Hawzen <hawzen@users.noreply.github.com>
2021-11-05 00:30:56 +0300

    Add prophet

--- model/utilities.py
+++ model/utilities.py
@@ -10,11 +10,11 @@ def preprocess_df(df, start_halving=2):
     df: input data
     start_halving: where to start off time series data according to bitcoin halving dates (0-4)
     """
-    df["Time"].astype("datetime64")
+    df["Time"] = df["Time"].astype("datetime64")
 
     epoch_to_date = {
         1: "2012-11-29",
-        2: "2016=07-10",
+        2: "2016-07-10",
         3: "2020-05-12",
     }
 
@@ -22,3 +22,7 @@ def preprocess_df(df, start_halving=2):
         df = df[df["Time"] > epoch_to_date[start_halving]]
 
     return df
+
+def preprocess_for_prophet(df, predicted_col="day_close"):
+    df[["ds", "y"]] = df[["Time", predicted_col]]
+    return df[["ds", "y"]]
